c8c5c9658b8669becfcb452a662d9c4456e674ea
pamplona-c <gustavo.pamplona.549@gmail.com>
2024-04-22 12:27:46 -0300

    :construction: Screen Welcome Concluida

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -1,12 +1,41 @@
-import { View, Text } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { View, Text, SafeAreaView, Image } from "react-native"
+import { TouchableOpacity } from "react-native-gesture-handler"
+
+
 
 
 export default function WelcomeScreen(){
+    navigation = useNavigation();
     return(
-        <View className="flex-1 items-center justify-center">
-            <Text>
-                Welcome
-            </Text>
-        </View>
+        <SafeAreaView className="flex-1 bg-black ">
+            <View className="flex-1 flex justify-around my-4">
+                <Text
+                className="text-3xl text-slate-200 font-bold text-center">
+                    Let's Get Started!
+                </Text>
+                <View className="flex-row justify-center"> 
+                    <Image source={require("../assets/welcoime.png")}
+                    style={{width:250, height:250}}/>
+                </View>
+
+                <View className="space-y-4">
+                    <TouchableOpacity onPress={()=> navigation.navigate("SignUp")}
+                    className="py-3 bg-yellow-400 mx-7 rounded-xl">
+                        <Text
+                        className="text-center font-bold  text-xl text-gray-700">
+                            Sign Up
+                        </Text>
+                    </TouchableOpacity>
+                    <View className="flex-row justify-center">
+                        <Text className="text-gray-700">Já possui conta </Text>
+                        <TouchableOpacity onPress={()=> navigation.navigate("Login")}>
+                            <Text className="text-yellow-400">Log in</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+
+        </SafeAreaView>
     )
 }
