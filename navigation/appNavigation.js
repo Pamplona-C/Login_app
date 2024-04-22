8b9502004b956a373fa7b9e361931a179d50fa98
pamplona-c <gustavo.pamplona.549@gmail.com>
2024-04-22 12:28:42 -0300

    HeaderShow:false

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -11,10 +11,10 @@ export default function AppNavigation() {
   return (
 
         <Stack.Navigator initialRouteName='Welcome'>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="SignUp" component={SignupScreen} />
+          <Stack.Screen name="Home" options={{headerShown:false}} component={HomeScreen} />
+          <Stack.Screen name="Welcome" options={{headerShown:false}} component={WelcomeScreen} />
+          <Stack.Screen name="Login" options={{headerShown:false}} component={LoginScreen} />
+          <Stack.Screen name="SignUp" options={{headerShown:false}} component={SignupScreen} />
         </Stack.Navigator>
 
   );
